013dea2c7d71f668ac13cc3248c9164abdf1538d
vionadjunaidy <144786402+vionadjunaidy@users.noreply.github.com>
2023-12-17 16:06:44 +0700

    Add files via upload

---
+++ script-sortbyRecipe.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    $(".buttons").click(function () {
+        var filter = $(this).attr("data-filter")
+        if (filter == "all") {
+            $(".box").show(400);
+        } else {
+            $(".box").not("." + filter).hide(200);
+            $(".box").filter("." + filter).show(400);
+        }
+    });
+});
+$(".dropdowncontent").hide();
+$(".drop").click(function () {
+    $(this).next(".dropdowncontent").slideToggle("slow");
+});
